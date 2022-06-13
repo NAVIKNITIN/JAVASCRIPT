f3f4a0c9629d43008c2e6e07c5f0ef2927415b72
nitinnavik <naviknitin143@gmail.com>
2022-06-13 14:20:50 +0530

    updates

--- chessboard.js
+++ chessboard.js
@@ -1,17 +1,17 @@
 // chessboard
 
-// let size = 8;
-// let board = "" ;
+let size = 8;
+let board = "" ;
 
-// for (let y =0; y<size; y=y+1){
-//     for(let x=0; x<size; x=x+1){
-//         if((x+y) % 2 ==0){
-//             board = board + " ";
-//         }else{
-//             board=board + "#";
+for (let y =0; y<size; y++){
+    for(let x=0; x<size; x++){
+        if((x+y) % 2 ==0){
+            board = board + " ";
+        }else{
+            board=board + "#";
 
-//         }   
-//     }
-//     board = board + "\n";
-// }
-// console.log(board);
+        }   
+    }
+    board = board + "\n";
+}
+console.log(board);
